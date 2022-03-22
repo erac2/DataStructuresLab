355e8e65951773f2d957aba0e15ee6541fe62c41
erac2 <71734077+erac2@users.noreply.github.com>
2022-03-21 23:31:52 -0400

    Add files via upload

--- BookException.java
+++ BookException.java
@@ -1,11 +1,13 @@
-package edu.monmouth.book;
+package edu.monmouth.hw3;
+import java.io.IOException;
+
 
 public class BookException extends Exception {
 
 	public BookException(){
 		super();
 	}
-	
+
 	public BookException(BookException e) {
 		super(e);
 	}
