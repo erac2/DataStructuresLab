ed0a20203fecf8ea0460bbfa4f29ddf48bb4c4f5
erac2 <71734077+erac2@users.noreply.github.com>
2022-04-21 18:38:56 -0400

    Add files via upload

--- Ascend.java
+++ Ascend.java
@@ -1,32 +1,31 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.*;
 
 public class Ascend {
-	
+
 	public static void main(String [] args) {
-	Integer [] arr1 = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
-	Integer [] arr2 = {2, 1, 4, 3, 9, 6};
-	
-	Arrays.sort(arr1.);
-	
-	int temp;
-	
-	arr2.sort;
-	
-		for(int i = 0; i<arr1.length; i++) {
-			for(int h = 0; h<arr2.length; h++) {
-				if(arr1[i] == arr2[h]) {
-					temp = arr1[i];
+		Integer [] arr1 = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
+		Integer [] arr2 = {2, 1, 4, 3, 9, 6};
+
+		//Arrays.sort(arr1.);
+
+		int temp;
+
+
+		for(int i = 0; i<arr2.length; i++) {
+			for(int h = 0; h<arr1.length; h++) {
+				if(arr2[i] == arr1[h]) {
+					temp = arr1[h];
 					System.out.print(temp + ", ");
-					for(int p = i+1; p<arr1.length; p++) {
-						if(arr1[p]==arr1[i]) {
-							temp = arr1[p];
-							System.out.print(temp + ", ");
-						}
-					}
+					
 				}
 			}
 		}
+		Arrays.sort(arr2);
+		System.out.println(arr2);
+
 	}
-	
+
+
+
 }
